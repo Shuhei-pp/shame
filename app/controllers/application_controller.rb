9e835b8a9e7125724c69b0e332db2b27acfb438d
Shuhei-pp <lotakotayota@gmail.com>
2021-07-21 19:08:49 +0900

    fixed commit 4ff785f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :session_user
+  #before_action :session_user
 
-  def session_user
-    @current_user = User.find_by(id: session[:user_id])
-  end
+  #def session_user
+  #  @current_user = User.find_by(id: session[:user_id])
+  #end
 end
